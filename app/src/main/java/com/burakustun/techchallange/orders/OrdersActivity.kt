244b9328a59534708e75dfb6b21239721415081a
Burak Üstün <brkstn34@gmail.com>
2019-09-19 21:06:58 +0300

    Logout and orders button added to OrdersActivity
    Logout imp.

--- app/src/main/java/com/burakustun/techchallange/orders/OrdersActivity.kt
+++ app/src/main/java/com/burakustun/techchallange/orders/OrdersActivity.kt
@@ -1,11 +1,16 @@
 package com.burakustun.techchallange.orders
 
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import com.burakustun.core.extensions.navigate
 import com.burakustun.core.extensions.showToast
-import com.burakustun.data.domain.ProductState
+import com.burakustun.core.utils.ClientPreferences
 import com.burakustun.techchallange.R
+import com.burakustun.techchallange.login.LoginActivity
+import kotlinx.android.synthetic.main.activity_orders.*
+import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 /**
@@ -14,6 +19,8 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 class OrdersActivity : AppCompatActivity() {
 
     private val viewModel: OrdersViewModel by viewModel()
+
+    private val clientPreferences: ClientPreferences by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +32,29 @@ class OrdersActivity : AppCompatActivity() {
     }
 
     private fun initListeners() {
-        
+        btnLogout.setOnClickListener {
+            showLogoutDialog()
+        }
+
+        btnOrders.setOnClickListener {
+            //TODO REFRESH ORDERS
+        }
+    }
+
+    private fun showLogoutDialog() {
+        val alertDialog: AlertDialog.Builder = AlertDialog.Builder(this)
+        alertDialog.setTitle(getString(R.string.dialog_logout_title))
+        alertDialog.setMessage(getString(R.string.dialog_logout_message))
+        alertDialog.setCancelable(false)
+        alertDialog.setPositiveButton(getString(R.string.dialog_logout_positive_button_title)) { _, _ ->
+            clientPreferences.setRememberMe(false)
+            navigate(this@OrdersActivity, LoginActivity::class.java)
+            finish()
+        }
+        alertDialog.setNegativeButton(R.string.dialog_logout_negative_button_title) { dialogInterface, _ ->
+            dialogInterface.dismiss()
+        }
+        alertDialog.show()
     }
 
     private fun initObservers() {
